107eb0b8b8b452a66d3079f05584c7bf34f5e86d
kemaldenizl <kemaldenizbum@gmail.com>
2026-06-03 14:35:51 +0300

    Setup file structure.

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function RootErrorPage() {
+  return (
+    <>
+      Root Error Page!!
+    </>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function RootNotFoundPage() {
+  return (
+    <>
+      Root Not Found Page!!
+    </>
+  );
+}
